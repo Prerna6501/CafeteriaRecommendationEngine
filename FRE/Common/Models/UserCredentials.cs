231bcf06d01f7d1997ad353dd2481b30c4afe672
Prerna Mehta <prerna.mehta@intimetec.com>
2024-07-14 15:14:18 +0530

    added user credential model

---
+++ FRE/Common/Models/UserCredentials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class UserCredentials
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
