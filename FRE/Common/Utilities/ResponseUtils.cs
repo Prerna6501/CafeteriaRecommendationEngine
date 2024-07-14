70454c462ead76710ffbee73253af564845c48b8
Prerna Mehta <prerna.mehta@intimetec.com>
2024-07-14 20:22:42 +0530

    Utilies for sending the  response

---
+++ FRE/Common/Utilities/ResponseUtils.cs
@@ -0,0 +1,28 @@
+﻿using Common.Models;
+using System.Text.Json;
+
+namespace Common.Utilities
+{
+    public static class ResponseUtils
+    {
+        public static string CreateSuccessJsonResponse(string response)
+        {
+            var responseModel = new ResponseModel()
+            {
+                Response = response,
+                IsSuccesful = true,
+            };
+            return JsonSerializer.Serialize(responseModel);
+        }
+
+        public static string CreateExceptionJsonResponse(string response)
+        {
+            var responseModel = new ResponseModel()
+            {
+                Response = response,
+                IsSuccesful = false,
+            };
+            return JsonSerializer.Serialize(responseModel);
+        }
+    }
+}
